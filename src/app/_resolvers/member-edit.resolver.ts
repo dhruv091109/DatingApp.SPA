ad5ed0ba6f57579e9a093087d6bc9e2c87f8cd76
dhruv091109 <dhruvthenorthstar@gmail.com>
2018-04-24 11:27:47 -0400

    update user profile

--- src/app/_resolvers/member-edit.resolver.ts
+++ src/app/_resolvers/member-edit.resolver.ts
@@ -5,14 +5,18 @@ import { AlertifyService } from '../_services/alertify.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import { AuthService } from '../_services/auth.service';
 
 @Injectable()
-export class MemberDetailResolver implements Resolve<User> {
+export class MemberEditResolver implements Resolve<User> {
 
-    constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
+    constructor(private userService: UserService,
+        private router: Router,
+        private alertify: AlertifyService,
+        private authService: AuthService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User> {
-        return this.userService.getUser(route.params['id']).catch(error => {
+        return this.userService.getUser(this.authService.decodedToken.nameid).catch(error => {
             this.alertify.error('problem in retriving in data');
             this.router.navigate(['/members']);
             return Observable.of(null);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,9 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
 import { NgxGalleryModule } from 'ngx-gallery';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 @NgModule({
@@ -38,7 +41,8 @@ import { NgxGalleryModule } from 'ngx-gallery';
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
 ],
   imports: [
     BrowserModule,
@@ -56,7 +60,9 @@ import { NgxGalleryModule } from 'ngx-gallery';
     AuthGuard,
     UserService,
     MemberDetailResolver,
-    MemberListResolver
+    MemberListResolver,
+    MemberEditResolver,
+    PreventUnsavedChanges
   ],
   bootstrap: [AppComponent]
 })
